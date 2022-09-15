787cad64fc3bccdea2cec786ab1073d32e1e134f
rashadaziz <rashad.aziz@ui.ac.id>
2022-09-15 23:40:43 +0700

    bismillah v2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          This is staging with custom domain!!
+          This is staging with custom domain pasti!!!
         </h1>
 
         <p className={styles.description}>
